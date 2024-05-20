752e6b67a06fa598c9c0f01bd183ff93ac4a7333
Franki99 <ntaganiradivanni@gmail.com>
2024-05-20 11:25:53 +0300

    added deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,35 +3,35 @@ import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
-    const {logout} = useLogout()
-    const {user} = useAuthContext()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
-    const handleClick = () => {
-        logout()
-    }
+  const handleClick = () => {
+    logout();
+  };
 
-    return(
-        <header>
-            <div className="container">
-                <Link to='/'>
-                    <h1>Keep your notes</h1>
-                </Link>
-                <nav>
-                    {user &&(
-                    <div>
-                        <button onClick={handleClick}>Log out</button>
-                    </div>
-                    )}
-                    {!user &&(
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
-                    </div>
-                    )}
-                </nav>
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Keep your notes</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <button onClick={handleClick}>Log out</button>
             </div>
-        </header>
-    )
-}
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
